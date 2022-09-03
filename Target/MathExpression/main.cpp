d68b048f6ad54f3a0889cbfd2fc87a155a05b7a6
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-09-03 17:09:38 +0700

    math expression

---
+++ Target/MathExpression/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  MathExpression
+//
+//  Created by Tommy-a on 03/09/22.
+//
+
+#include <iostream>
+
+int main(){
+    int a = 1 + 2 ; // 3
+    int b = 2 - 1 ; // 1
+    int c = 1 / 1 ; // 1
+    int d = 1 * 1 ; // 1
+    int e = 3 % 2 ; // 1
+    int f = a++ ; // f = 3, a = 4
+    int g = ++b ; // g = 2, b = 2
+
+    std::cout << f << " " << a << std::endl;
+    std::cout << g << " " << b << std::endl;
+
+}
