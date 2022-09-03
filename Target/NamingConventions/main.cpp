d883a81ee2adeaf86e4bae10af246d2a8f3530ac
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-09-03 16:57:31 +0700

    naming convention

---
+++ Target/NamingConventions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NamingConventions
+//
+//  Created by Tommy-a on 03/09/22.
+//
+
+#include <iostream>
+
+int main(){
+    int file_size; // snake case
+    int FileSize; // pascal case
+    int fileSize; // camel case
+    int iFileSize; // hungarian notation
+}
