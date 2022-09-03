11b5362a31e4f2c4d329f9d547ba8d94419b22ea
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-09-03 16:37:18 +0700

    change folder

---
+++ Target/Constants/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Constants
+//
+//  Created by Tommy-a on 03/09/22.
+//
+
+#include <iostream>
+
+int main() {
+    const double pi = 3.14;
+    
+    std::cout << pi << std::endl;
+}
