ab2f5aac77107d96fd6a9b480a670e2f2f7a806c
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-09-03 17:43:09 +0700

    reading input

---
+++ Target/Reading/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Reading
+//
+//  Created by Tommy-a on 03/09/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout << "kasih nilai a dan b: ";
+    cin >> a >> b;
+    cout << a << " and " << b << endl;
+    
+    return 0;
+}
