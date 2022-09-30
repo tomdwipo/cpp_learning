87c46c27a5349158cf45669eda33e48ffc1e9c45
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-09-30 18:12:51 +0700

    boolean alpha

---
+++ Target/boolean/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  boolean
+//
+//  Created by Tommy-a on 30/09/22.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    bool boolean = true;
+    cout << boolalpha << boolean << endl;
+    cout << noboolalpha << boolean << endl;
+    return 0;
+}
