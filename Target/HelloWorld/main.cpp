c70c63d811741be09a2c07d77f1f41496e9bd442
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2025-10-31 21:08:20 +0700

    commit

--- Target/HelloWorld/main.cpp
+++ Target/HelloWorld/main.cpp
@@ -6,10 +6,7 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(){
-    int file_size = 100;
-    double sales = 9.99;
-    std::cout << "Hello World! " << file_size << " " << sales << std::endl;
-    return 0;
+    std::cout << "Halo, dunia!" << std::endl;
 }
